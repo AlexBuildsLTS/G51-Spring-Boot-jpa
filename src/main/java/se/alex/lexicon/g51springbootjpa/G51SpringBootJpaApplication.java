5e00cdeced4c49d27b620a3c6bb0646fac3473db
Alex Youssef <Alex.youssef@live.com>
2024-09-17 09:43:01 +0200

    Initial commit

---
+++ src/main/java/se/alex/lexicon/g51springbootjpa/G51SpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package se.alex.lexicon.g51springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G51SpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G51SpringBootJpaApplication.class, args);
+    }
+
+}
